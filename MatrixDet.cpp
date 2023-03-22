41d4f46eff1b5b937cc6888bcf0b3a8a390fe724
D3Falcon <99274739+D3Falcon@users.noreply.github.com>
2023-03-22 12:39:07 -0300

    Added new functions

--- MatrixDet.cpp
+++ MatrixDet.cpp
@@ -6,13 +6,15 @@
 using namespace std;
 
 /*O propósito desse programa é calcular o determinante de matrizes com imput do user.
-Primeiro, o programa pede para entrar com a ordem da matriz e em seguida os elementos.
+Primeiro, o programa pede para informar a ordem da matriz e em seguida os elementos.
 O resultado é o determinante já calculado. O programa repete até que o user entre com
 o número zero, como ordem de matriz para encerrar.*/
 
 
 int det_2 (int M2[][2]);
 int det_3 (int M[][9], int x);
+void get_element (int M[][9], int x);
+void printM (int M[][9], int x);
 
 int main() {
     int M[9][9], M2[2][2];
@@ -20,10 +22,10 @@ int main() {
     char a = '0';
     do {
         do {
-            //Pedido de elemento, ele entra como char e é modificado para interiro.
+            //Pedido da ordem, ela entra como char e é modificado para interiro.
             cout << "Enter matrix order\n0 kills program\n";
             cin >> a;
-        } while (!isdigit(a) && a <= '0' && a > 10);
+        } while (!isdigit(a) && a <= '0' && a > '9');
         x = a - 48;
         int det = 0;
 
@@ -33,7 +35,6 @@ int main() {
             cout << "Det = " << det << ".\n";
             } 
         else if (x==2) {
-            //Aqui é o pedido de elementos da matrix a ser calculada. Essa chamada vai se repetir em todas as ordens, até que eu faça uma função com esse propósito.
             for (int i = 0; i < x; i++) {
                 for (int j = 0; j < x; j++) {
                        cout << "Enter matrix element a" << i+1 << j+1 << "\n ";
@@ -43,47 +44,29 @@ int main() {
             det = det_2 (M2);
             cout << "Det = " << det << ".\n";
 
-        } else if (x==3) {
-            for (int i = 0; i < x; i++) {
-                for (int j = 0; j < x; j++) {
-                       cout << "Enter matrix element a" << i+1 << j+1 << "\n ";
-                       cin >> M[i][j];
-                }
-            }
-            det = det_3 (M, x);
-            cout << "Det = " << det << ".\n";
-        } else if (x>3 && x<10) {
-            for (int i = 0; i < x; i++) {
-                for (int j = 0; j < x; j++) {
-                       cout << "Enter matrix element a" << i+1 << j+1 << "\n ";
-                       cin >> M[i][j];
-                }
-            }
-            
+        } else if (x>2 && x<10) {
+            get_element(M, x);
+            printM (M, x);
             det = det_3 (M, x);
             cout << "Det = " << det << ".\n";
         } else if (x > 9) {
             cout << "We don't have support for this order yet. New versions comming soon.\n";
         }
-        cout << " " << x << "\n";
     } while (x!=0);  
     return 0;
 }
 
-int det_2 (int M[][2])
-{
+int det_2 (int M[][2]) {
     int det2 = 0;
     det2 = M[0][0] * M[1][1] - M[0][1] * M[1][0];
     return det2;
 }
 
-int det_3 (int M[][9], int x) 
-{
+int det_3 (int M[][9], int x) {
     //Essa função cria submatrizes para serem utilizadas na função de ordem 2.
     int det = 0;
     int subM[2][2];
-    if (x==3) 
-    {
+    if (x==3) {
         for (int i = 0; i < 3; i++) {
             for ( int j = 1; j < 3; j++){
                 //Aqui usamos 3 loops encadeados e um contador para criar as submatrizes.
@@ -99,8 +82,7 @@ int det_3 (int M[][9], int x)
         }
         return det;
     }
-    else
-    {
+    else {
         int subM[9][9];
         for (int i = 0; i < x; i++){
             for ( int j = 1; j < x; j++){
@@ -108,13 +90,36 @@ int det_3 (int M[][9], int x)
                 for (int k = 0; k < x; k++){
                      if (k != i){
                         subM[j-1][countk] = M[j][k]; 
-                         countk++;
+                        countk++;
                     }
                 }
             }
         det += pow (-1, i+2) * M[0][i] * det_3(subM, x-1);
         }
         return det; 
+    }  
+}
+
+void get_element (int M[][9], int x) {
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < x; j++) {
+            cout << "Enter matrix element a" << i+1 << j+1 << "\n ";
+             cin >> M[i][j];
+        }
     }
-    
+}
+
+void printM (int M[][9], int x) {
+    cout << "\n";
+    for (int i = 0; i < x; i++) {
+        cout << "|";
+        for (int j = 0; j < x; j++) {
+            if (M[i][j]>=0 && M[i][j]<10){
+                cout << " ";
+            }
+            cout << M[i][j] << " ";
+            }   
+        cout << "|\n";
+        }
+    cout << "\n";
 }
